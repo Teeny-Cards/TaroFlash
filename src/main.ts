3e7ba703092da7dd3ebf4e3353b55588f4d78a41
Chris Jol <chris@chriss-air.lan>
2024-01-05 16:31:39 -0800

    install firebase & tailwind, cleanup boilerplate

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDCXN86DZJ1VOLuRSPPUw4ClbF8uqQ908E',
+  authDomain: 'tinycards-9b4f2.firebaseapp.com',
+  projectId: 'tinycards-9b4f2',
+  storageBucket: 'tinycards-9b4f2.appspot.com',
+  messagingSenderId: '1067245046547',
+  appId: '1:1067245046547:web:e60bfc87982cd49bc2e8e6',
+  measurementId: 'G-BC90DCFVZR'
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+const analytics = getAnalytics(firebaseApp)
+
 const app = createApp(App)
 
 app.use(createPinia())
