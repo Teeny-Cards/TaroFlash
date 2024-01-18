71afff7923ff48237d58e6f1c0a57be663e668b8
Chris Jol <chris@chriss-air.lan>
2024-01-18 11:14:09 -0800

    Write TeenyCard Tests

---
+++ src/components/TeenyCard/TeenyCard.test.ts
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils'
+import TeenyCard from './TeenyCard.vue'
+import { expect, test } from 'vitest'
+
+test('It renders slot content', async () => {
+  const wrapper = mount(TeenyCard, {
+    slots: {
+      default: '<div class="test">Test</div>'
+    }
+  })
+
+  expect(wrapper.html()).toContain('<div class="test">Test</div>')
+})
+
+test('It renders a default size when no `size` prop is passed', async () => {
+  const wrapper = mount(TeenyCard)
+
+  expect(wrapper.classes()).toContain('w-card-base')
+})
+
+test('It renders small when passing `small` to the `size` prop', async () => {
+  const wrapper = mount(TeenyCard, {
+    props: { size: 'small' }
+  })
+
+  expect(wrapper.classes()).toContain('w-card-small')
+})
+
+test('It renders large when passing `large` to the `size` prop', async () => {
+  const wrapper = mount(TeenyCard, {
+    props: { size: 'large' }
+  })
+
+  expect(wrapper.classes()).toContain('w-card-large')
+})
