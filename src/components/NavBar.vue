d66152fc5cd97c650e9a9597bc9e8fb920701a05
Chris Jol <chris@chriss-air.lan>
2024-03-01 20:12:43 -0800

    add color palette and primary font

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="w-full bg-green-400 sticky top-0 z-10">
-    <div class="w-full max-w-screen-xl mx-auto px-8 py-3 flex justify-between items-center">
-      <RouterLink to="/dashboard" class="text-xl font-semibold text-white">Teeny Cards</RouterLink>
+  <nav class="w-full bg-blue font-primary sticky top-0 z-10">
+    <div class="w-full max-w-app mx-auto px-8 py-4 flex justify-between items-center">
+      <RouterLink to="/dashboard" class="text-4xl text-white">TeenyCards</RouterLink>
       <div class="flex gap-3 items-center">
         <RouterLink to="/dashboard" class="text-white">Dashboard</RouterLink>
         <RouterLink to="/create" class="text-white">Create</RouterLink>
