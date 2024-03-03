4c165ad9536524fc868be1587981f2ed13c17405
Chris Jol <chris@chriss-air.lan>
2024-03-03 14:06:59 -0800

    redesign: convert teeny-icon to use map

--- src/components/TeenyComponents/TeenyIcon.vue
+++ src/components/TeenyComponents/TeenyIcon.vue
@@ -1,37 +1,30 @@
 <template>
-  <div teeny-icon :class="color">
-    <Icon :class="size"></Icon>
+  <div teeny-icon>
+    <Icon :class="iconSize[size]"></Icon>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, defineAsyncComponent } from 'vue'
+import { defineAsyncComponent } from 'vue'
 
 const props = defineProps({
   src: {
     type: String,
     required: true
   },
-  fill: String,
-  size: String
+  size: {
+    type: String,
+    validator: (value: string) => ['large', 'base', 'small', 'teeny'].includes(value),
+    default: 'base'
+  }
 })
 
 const Icon = defineAsyncComponent(() => import(`../../assets/svgs/${props.src}.svg`))
 
-const color = {
-  'text-gray': props.fill === 'grey'
+const iconSize: { [key: string]: string } = {
+  large: 'w-6 h-6',
+  base: 'w-5 h-5',
+  small: 'w-4 h-4',
+  teeny: 'w-3 h-3'
 }
-
-const size = computed(() => {
-  switch (props.size) {
-    case 'large':
-      return 'w-6 h-6'
-    case 'small':
-      return 'w-4 h-4'
-    case 'teeny':
-      return 'w-3 h-3'
-    default: // base
-      return 'w-5 h-5'
-  }
-})
 </script>
