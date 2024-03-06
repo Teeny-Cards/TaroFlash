e3722ddb93f9c098c9a294519815aa169ec7c255
Chris Jol <chris@chriss-air.lan>
2024-03-05 18:27:35 -0800

    redesign: animate dropdown

--- src/components/TeenyDropdown.vue
+++ src/components/TeenyDropdown.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="relative" ref="teenyDropdown">
-    <slot name="trigger" :toggleDropdown="toggleDropdown" :open="dropdownVisible">
-      <TeenyButton :variant="variant" :inverted="inverted" @click="toggleDropdown" />
+  <div teeny-dropdown class="relative" ref="teenyDropdown">
+    <slot
+      teeny-dropdown__trigger
+      name="trigger"
+      :toggleDropdown="toggleDropdown"
+      :open="dropdownVisible"
+    >
+      <TeenyButton
+        teeny-dropdown-trigger
+        :variant="variant"
+        :inverted="inverted"
+        @click="toggleDropdown"
+      />
     </slot>
     <div v-if="dropdownVisible" class="absolute z-10 right-0 top-full mt-2">
       <slot name="dropdown">
         <div class="flex flex-col gap-1.5 items-end">
           <TeenyButton
             v-for="action in actions"
-            class="shadow"
+            teeny-dropdown__action
+            class="shadow opacity-0 transition-all duration-200 transform scale-75"
             :key="action.label"
             :variant="action.variant"
             :inverted="action.inverted"
@@ -30,6 +41,7 @@
 import TeenyButton from '@/components/TeenyButton.vue'
 import { onUnmounted, ref } from 'vue'
 import { onMounted } from 'vue'
+import { nextTick } from 'vue'
 
 export interface DropdownAction {
   label: string
@@ -59,8 +71,21 @@ onUnmounted(() => {
 const teenyDropdown = ref<HTMLDivElement>()
 const dropdownVisible = ref(false)
 
-function toggleDropdown(): void {
-  dropdownVisible.value = !dropdownVisible.value
+async function toggleDropdown(): Promise<void> {
+  if (dropdownVisible.value) {
+    const nodeList = teenyDropdown.value?.querySelectorAll('[teeny-dropdown__action]')
+    const actions = nodeList ? [...nodeList] : []
+
+    await animateActionsOut(actions, 25)
+    dropdownVisible.value = false
+  } else {
+    dropdownVisible.value = true
+    await nextTick()
+
+    const nodeList = teenyDropdown.value?.querySelectorAll('[teeny-dropdown__action]')
+    const actions = nodeList ? [...nodeList] : []
+    animateActionsIn(actions, 50)
+  }
 }
 
 function closeDropdown(e: MouseEvent): void {
@@ -69,5 +94,33 @@ function closeDropdown(e: MouseEvent): void {
   if (!teenyDropdown.value?.contains(clickedElement)) {
     dropdownVisible.value = false
   }
+}
+
+function animateActionsIn(elements: Element[], delay: number): void {
+  elements.forEach((element, index) => {
+    setTimeout(() => {
+      element.classList.remove('opacity-0')
+      element.classList.remove('scale-75')
+    }, delay * index)
+  })
+}
+
+function animateActionsOut(elements: Element[], delay: number): Promise<void> {
+  return new Promise((resolve) => {
+    for (let i = elements.length - 1; i >= 0; i--) {
+      setTimeout(
+        () => {
+          elements[i].classList.add('opacity-0')
+          elements[i].classList.add('scale-75')
+
+          // Resolve the promise when the last element has received the class
+          if (i === 0) {
+            setTimeout(resolve, 200)
+          }
+        },
+        delay * (elements.length - i)
+      )
+    }
+  })
 }
 </script>
