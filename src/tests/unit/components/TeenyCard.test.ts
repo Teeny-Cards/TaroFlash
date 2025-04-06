9c993064b9aea5892f28e6cc2c591b7549867a14
Chris <jol.chris2@gmail.com>
2025-04-06 13:52:35 -0700

    remove old tests

--- src/tests/unit/components/TeenyCard.test.ts
+++ src/tests/unit/components/TeenyCard.test.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import TeenyCard from '@/components/TeenyCard.vue'
+import TeenyCard from '@teeny/TeenyCard.vue'
 import { expect, test } from 'vitest'
 
 test('It renders slot content', async () => {
@@ -10,26 +10,4 @@ test('It renders slot content', async () => {
   })
 
   expect(wrapper.html()).toContain('<div class="test">Test</div>')
-})
-
-test('It renders a default size when no `size` prop is passed', async () => {
-  const wrapper = mount(TeenyCard)
-
-  expect(wrapper.classes()).toContain('w-card-base')
-})
-
-test('It renders small when passing `small` to the `size` prop', async () => {
-  const wrapper = mount(TeenyCard, {
-    props: { size: 'small' }
-  })
-
-  expect(wrapper.classes()).toContain('w-card-small')
-})
-
-test('It renders large when passing `large` to the `size` prop', async () => {
-  const wrapper = mount(TeenyCard, {
-    props: { size: 'large' }
-  })
-
-  expect(wrapper.classes()).toContain('w-card-large')
 })
