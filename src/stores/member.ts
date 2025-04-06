46afc612356c3e253b294b0b07905bd82f77d2ea
Chris <jol.chris2@gmail.com>
2025-04-06 15:16:23 -0700

    more cleanup

--- src/stores/member.ts
+++ src/stores/member.ts
@@ -1,42 +1,32 @@
 import { fetchMemberById } from '@/services/memberService'
 import { defineStore } from 'pinia'
-import { useSessionStore } from './session'
-import Logger from '@/utils/logger'
 
 export const useMemberStore = defineStore('member', {
   state: () => ({
-    display_name: '',
-    id: '',
-    description: '',
-    created_at: '',
-    num_decks: 0
+    member: null as Member | null
   }),
 
   actions: {
-    async fetchMember(): Promise<void> {
-      const session = useSessionStore()
+    async fetchMember(user_id: string): Promise<void> {
+      this.member = await fetchMemberById(user_id)
+    }
+  },
 
-      try {
-        const member = await fetchMemberById(session.user?.id ?? '')
-        this.setMember(member)
-      } catch (error: any) {
-        Logger.error(`Error fetching member: ${error.message}`)
-        throw error
-      }
+  getters: {
+    id({ member }): string {
+      return member?.id ?? ''
     },
-
-    setMember(member: Member | null): void {
-      if (!member) {
-        Logger.warn('Attempted to set null member')
-        return
-      }
-
-      Logger.debug(`Setting member data for: ${member.display_name}`)
-      this.id = member.id ?? ''
-      this.display_name = member.display_name ?? ''
-      this.description = member.description ?? ''
-      this.created_at = member.created_at ?? ''
-      this.num_decks = member.num_decks ?? 0
+    display_name({ member }): string {
+      return member?.display_name ?? ''
+    },
+    created_at({ member }): string {
+      return member?.created_at ?? ''
+    },
+    description({ member }): string {
+      return member?.description ?? ''
+    },
+    num_decks({ member }): number {
+      return member?.num_decks ?? 0
     }
   }
 })
