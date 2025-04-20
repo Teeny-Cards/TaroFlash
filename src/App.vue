9dde80c437ec2c9672c84284bf1802c07c3da8d6
Chris <jol.chris2@gmail.com>
2025-04-20 16:29:37 -0700

    remove toast & alert managers

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <RouterView />
-  <ui-kit:toast-manager />
-  <ui-kit:alert-manager />
+
+  <teleport to="[toast-container]">
+    <ui-kit:toast v-for="(toast, index) in toasts" :key="index" :toast="toast" />
+  </teleport>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useToastStore } from '@/stores/toast'
+
+const toastStore = useToastStore()
+const { toasts } = storeToRefs(toastStore)
 </script>
