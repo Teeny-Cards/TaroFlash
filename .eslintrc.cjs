c3608d9ee91df319d2692e8a60749dd89191dd37
Chris <jol.chris2@gmail.com>
2025-06-24 09:17:19 -0700

    add intl

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,13 +7,22 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:@intlify/vue-i18n/recommended-legacy'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   rules: {
     'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@intlify/vue-i18n/no-dynamic-keys': 'error',
+    '@intlify/vue-i18n/no-raw-text': 'error',
+    '@intlify/vue-i18n/no-unused-keys': [
+      'error',
+      {
+        extensions: ['.js', '.vue', '.ts']
+      }
+    ],
     'vue/multi-word-component-names': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,25 @@ import App from './App.vue'
 import router from './router'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+import enUS from './translations/en-us'
 import Logger from './utils/logger'
 import registerUIKitComponents from './components/ui-kit/_index'
 
 Logger.setLogLevel(import.meta.env.VITE_LOG_LEVEL)
 
+const i18n = createI18n({
+  locale: 'en-us',
+  legacy: false,
+  messages: {
+    'en-us': enUS
+  }
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(i18n)
 app.use(router)
 
 registerUIKitComponents(app)
