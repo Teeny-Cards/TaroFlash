49e42e8b772738c600c75651116b5c79e352050c
Chris <jol.chris2@gmail.com>
2025-06-24 08:54:56 -0700

    move tests outside of src

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      setupFiles: ['./src/tests/setup.js'],
+      setupFiles: ['./tests/setup.js'],
       coverage: {
         enabled: true,
         reporter: ['text', 'json', 'html', 'json-summary'],
