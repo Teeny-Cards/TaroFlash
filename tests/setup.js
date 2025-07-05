13d52c86d7efdd6a57acf9ab94d1836074b40f0a
Chris <jol.chris2@gmail.com>
2025-07-05 12:14:30 -0700

    properly setup intl

--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,20 @@
 // test/setup.ts
 import { config } from '@vue/test-utils'
 import registerUIKitComponents from '@/components/ui-kit/_index'
-
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+import messages from '@intlify/unplugin-vue-i18n/messages'
 
-// Fake app just to register globally
+const i18n = createI18n({
+  locale: 'en-us',
+  legacy: false,
+  messages
+})
+
 const app = createApp({})
+app.use(i18n)
 registerUIKitComponents(app)
 
 // Copy the globally registered components into test-utils config
 config.global.components = app._context.components
+config.global.plugins = [i18n]
