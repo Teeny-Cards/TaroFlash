450ddf3524a1f57b0e0a948d6e57fe44ec8d9834
Chris <jol.chris2@gmail.com>
2025-07-19 22:10:01 -0700

    TNY-21: border animation

--- src/components/ui-kit/alert.vue
+++ src/components/ui-kit/alert.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
+import { computed } from 'vue'
 
 export type AlertType = 'warn' | 'info'
 
-defineProps<{
+const { cancelLabel, confirmLabel } = defineProps<{
   cancelLabel?: string
   confirmLabel?: string
   message?: string
@@ -14,13 +15,34 @@ defineProps<{
 
 const { t } = useI18n()
 
-const buttonClasses =
-  'group relative w-full cursor-pointer p-4 text-lg hover:text-blue-500 text-brown-700'
+const cancelText = computed(() => {
+  return cancelLabel ?? t('common.cancel')
+})
+
+const confirmText = computed(() => {
+  return confirmLabel ?? t('common.continue')
+})
+
+const buttonClasses = 'group relative w-full cursor-pointer p-4 text-lg text-brown-700'
+
 const buttonHoverClasses = `
-  rounded-2 absolute -inset-0.5 flex border-3 border-blue-500
+  rounded-2 absolute -inset-1 flex
   opacity-0 transition-[all] duration-100 ease-in-out
   group-hover:opacity-100 group-focus:opacity-100 focus:outline-none
+  flex items-center justify-center p-0.75 bg-size-[400%_400%]
   `
+
+const spanClasses = `bg-white w-full h-full rounded-1.5 flex items-center justify-center`
+
+const borderColor: { [key in AlertType]: string } = {
+  warn: 'bg-linear-to-br from-red-600 from-30% via-red-300 via-50% to-red-600 to-70%',
+  info: 'bg-linear-to-br from-blue-500 from-40% via-blue-400 via-50% to-blue-500 to-80%'
+}
+
+const textColor: { [key in AlertType]: string } = {
+  warn: 'text-red-500',
+  info: 'text-blue-500'
+}
 </script>
 
 <template>
@@ -39,16 +61,27 @@ const buttonHoverClasses = `
         :class="buttonClasses"
         @click="() => close(false)"
       >
-        {{ cancelLabel ?? t('common.cancel') }}
-        <div :class="buttonHoverClasses" />
+        {{ cancelText }}
+        <div :class="buttonHoverClasses" class="bg-grey-300 text-grey-500 animate-bg-slide">
+          <span :class="spanClasses">
+            {{ cancelText }}
+          </span>
+        </div>
       </button>
       <button
         data-testid="confirmation-alert__confirm"
         :class="buttonClasses"
         @click="() => close(true)"
       >
-        {{ confirmLabel ?? t('common.continue') }}
-        <div :class="buttonHoverClasses" />
+        {{ confirmText }}
+        <div
+          :class="[buttonHoverClasses, borderColor[type ?? 'warn'], textColor[type ?? 'warn']]"
+          class="animate-bg-slide"
+        >
+          <span :class="spanClasses">
+            {{ confirmText }}
+          </span>
+        </div>
       </button>
     </div>
   </div>
