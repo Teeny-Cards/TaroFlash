277a6e8a7a59ce37f6edd91e7ee3c0e3d32e4d4e
Chris <jol.chris2@gmail.com>
2025-07-23 13:16:31 -0700

    TNY-27: Migrate tooltip to floating-ui

--- src/components/ui-kit/tooltip.vue
+++ src/components/ui-kit/tooltip.vue
@@ -1,44 +1,57 @@
 <script setup lang="ts">
-import { ref, onMounted, useTemplateRef } from 'vue'
+import { computed, useTemplateRef, onMounted } from 'vue'
+import { useFloating, shift, flip, autoUpdate, offset, type Placement } from '@floating-ui/vue'
 
-const { text, position = 'top-right' } = defineProps<{
+const { text, placement: placement_prop = 'top' } = defineProps<{
   text: string
-  position?:
-    | 'top'
-    | 'bottom'
-    | 'left'
-    | 'right'
-    | 'top-right'
-    | 'bottom-right'
-    | 'top-left'
-    | 'bottom-left'
+  placement?: Placement
 }>()
 
-const tooltip = useTemplateRef('ui-kit-tooltip')
-
 onMounted(() => {
-  if (tooltip.value) {
-    const parent = tooltip.value.parentElement
+  const parent = triggerRef.value?.parentElement
 
-    if (parent) {
-      parent.classList.add('group', 'relative')
-    }
+  if (parent && !(parent?.style?.position ?? '' in ['absolute', 'relative', 'fixed'])) {
+    parent.classList.add('relative')
   }
 })
+
+const popoverRef = useTemplateRef('popoverRef')
+const triggerRef = useTemplateRef('triggerRef')
+
+const { placement, floatingStyles } = useFloating(triggerRef, popoverRef, {
+  placement: placement_prop,
+  strategy: 'fixed',
+  whileElementsMounted: autoUpdate,
+  middleware: [
+    offset(() => 24),
+    shift({ padding: 24 }),
+    flip({
+      fallbackPlacements: ['right', 'left', 'bottom']
+    })
+  ]
+})
+
+const side = computed(() => placement?.value.split('-')[0] ?? 'right')
 </script>
 
 <template>
   <span
-    data-testid="ui-kit-tooltip"
-    class="ui-kit-tooltip hidden group-hover:flex"
-    :class="`tooltip--${position}`"
-    ref="ui-kit-tooltip"
-    role="tooltip"
+    ref="triggerRef"
+    data-testid="ui-kit-tooltip-container"
+    class="ui-kit-tooltip-container group"
   >
-    <div class="ui-kit-tooltip__line"></div>
-
-    <div class="ui-kit-tooltip__bubble">
-      <p class="text-sm text-purple-500">{{ text }}</p>
-    </div>
+    <span
+      ref="popoverRef"
+      data-testid="ui-kit-tooltip"
+      class="ui-kit-tooltip"
+      :class="`ui-kit-tooltip--${side}`"
+      :style="floatingStyles"
+      role="tooltip"
+    >
+      <div data-testid="ui-kit-tooltip__bubble" class="ui-kit-tooltip__bubble">
+        <p class="text-sm text-purple-500">{{ text }}</p>
+        <div data-testid="ui-kit-tooltip__arrow" class="ui-kit-tooltip__arrow"></div>
+      </div>
+    </span>
   </span>
 </template>
