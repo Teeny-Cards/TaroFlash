b0b977b63fa0fd7da7985e135fce532ff47f9649
Chris <jol.chris2@gmail.com>
2025-07-25 14:26:49 -0700

    TNY-34: Migrate styles for card component

--- src/components/card.vue
+++ src/components/card.vue
@@ -8,21 +8,11 @@
     leave-to-class="motion-safe:rotate-y-90 -translate-y-6"
     leave-active-class="motion-safe:transition-[all] ease-in-out duration-150"
   >
-    <div
-      v-if="revealed"
-      data-testid="card"
-      class="flex shrink-0 items-center justify-center p-3"
-      :class="[defaultClasses, sizeClasses[size], { 'bg-white': revealed }]"
-    >
+    <div v-if="revealed" data-testid="card" class="card card--revealed" :class="`card--${size}`">
       <slot></slot>
     </div>
-    <div
-      v-else
-      data-testid="card"
-      class="relative shrink-0 bg-green-400 p-3"
-      :class="[defaultClasses, sizeClasses[size]]"
-    >
-      <div v-if="image_url" class="absolute inset-0">
+    <div v-else data-testid="card" class="card card--hidden" :class="`card--${size}`">
+      <div v-if="image_url" class="card__image">
         <img :src="image_url" alt="Deck Image preview" class="h-full w-full object-cover" />
       </div>
       <slot name="back"></slot>
@@ -36,15 +26,4 @@ const { size = 'base' } = defineProps<{
   revealed?: Boolean
   image_url?: string
 }>()
-
-const defaultClasses = 'aspect-card'
-
-const sizeClasses: { [key: string]: string } = {
-  '3xs': 'min-w-7 max-w-7 rounded-2 text-sm',
-  '2xs': 'min-w-10.75 max-w-10.75 rounded-3 text-sm',
-  xs: 'min-w-25.5 max-w-25.5 rounded-6 text-base',
-  small: 'min-w-34.5 max-w-34.5 rounded-8 text-lg',
-  base: 'min-w-48 max-w-48 rounded-10 text-2xl',
-  large: 'min-w-65 max-w-65 rounded-12  text-3xl'
-}
 </script>
