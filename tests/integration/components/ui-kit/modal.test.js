cfa89acefaaed1ef6e82db73bdab682515b865a2
Chris <jol.chris2@gmail.com>
2025-07-26 12:39:01 -0700

    TNY-35: Deck Settings Tests

--- tests/integration/components/ui-kit/modal.test.js
+++ tests/integration/components/ui-kit/modal.test.js
@@ -1,4 +1,4 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import { expect, test, vi } from 'vitest'
 import Modal from '@/components/ui-kit/modal.vue'
 import { shallowRef, defineComponent, markRaw } from 'vue'
