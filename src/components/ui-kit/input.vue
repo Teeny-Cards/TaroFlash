3a2e458951a0d9cd1f42ffa9dad8ee4a65b3a30d
Chris <jol.chris2@gmail.com>
2025-07-28 12:54:08 -0700

    TNY-58: Build out modal

--- src/components/ui-kit/input.vue
+++ src/components/ui-kit/input.vue
@@ -1,29 +1,23 @@
 <template>
-  <div data-testid="ui-kit-input" class="ui-kit-input" :class="[textAlignClass, sizeClass]">
-    <input :placeholder="placeholder" v-model="value" />
-  </div>
+  <label
+    data-testid="ui-kit-input-container"
+    class="ui-kit-input-container"
+    :class="[`ui-kit-input-container--text-${textAlign}`, `ui-kit-input-container--${size}`]"
+  >
+    <span>{{ label }}</span>
+    <div data-testid="ui-kit-input" class="ui-kit-input">
+      <input :placeholder="placeholder" v-model="value" />
+    </div>
+  </label>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-
-const {
-  placeholder,
-  textAlign = 'left',
-  size = 'base'
-} = defineProps<{
+const { textAlign = 'left', size = 'base' } = defineProps<{
+  label?: string
   placeholder?: string
   textAlign?: 'left' | 'center' | 'right'
   size?: 'sm' | 'base' | 'lg'
 }>()
 
 const value = defineModel<string>('value')
-
-const textAlignClass = computed(() => {
-  return `ui-kit-input--text-${textAlign}`
-})
-
-const sizeClass = computed(() => {
-  return `ui-kit-input--${size}`
-})
 </script>
---
+++ src/components/ui-kit/tag.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <span data-testid="ui-kit-tag" class="bg-brown-100 rounded-4.5 px-4.5 py-2.5">
+    <p class="text-brown-700">
+      <slot></slot>
+    </p>
+  </span>
+</template>
