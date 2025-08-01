3c16c1b273a1004fb60d28fb967942c5b2079c19
Chris <jol.chris2@gmail.com>
2025-07-31 19:23:18 -0700

    colocate styles with components

--- src/components/ui-kit/toggle.vue
+++ src/components/ui-kit/toggle.vue
@@ -27,3 +27,31 @@ const checked = defineModel<boolean>('checked')
     </span>
   </label>
 </template>
+
+<style>
+@reference '@/styles/main.css';
+
+.ui-kit-toggle {
+  @apply flex cursor-pointer items-center justify-between gap-2;
+}
+
+.ui-kit-toggle .ui-kit-toggle__label {
+  @apply text-brown-700;
+}
+
+.ui-kit-toggle .ui-kit-toggle__switch {
+  @apply bg-brown-100 flex w-12 items-center rounded-full p-1 transition-colors;
+}
+
+.ui-kit-toggle.ui-kit-toggle--checked .ui-kit-toggle__switch {
+  @apply bg-green-400;
+}
+
+.ui-kit-toggle .ui-kit-toggle__switch-handle {
+  @apply bg-brown-500 h-5 w-5 rounded-full transition-all duration-100 ease-in-out;
+}
+
+.ui-kit-toggle.ui-kit-toggle--checked .ui-kit-toggle__switch-handle {
+  @apply bg-brown-100 translate-x-full transform;
+}
+</style>
