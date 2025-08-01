cba7e4817c402395c183429e1895c4f28381116b
Chris <jol.chris2@gmail.com>
2025-07-31 18:28:20 -0700

    add card grid

---
+++ src/components/views/deck-view/card-grid.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Card from '@/components/card.vue'
+
+defineProps<{
+  cards: Card[]
+}>()
+</script>
+
+<template>
+  <div class="grid auto-cols-[138px] grid-flow-col gap-4 pt-3">
+    <card v-for="card in cards" :key="card.id" size="small" :revealed="true">
+      {{ card.front_text }}
+    </card>
+  </div>
+</template>
