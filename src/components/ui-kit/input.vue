a246e8cf502e1e3f52051f7b608228b333940f35
Chris <jol.chris2@gmail.com>
2025-07-31 19:23:18 -0700

    colocate styles with components

--- src/components/ui-kit/input.vue
+++ src/components/ui-kit/input.vue
@@ -21,3 +21,47 @@ const value = defineModel<string>('value')
     </div>
   </label>
 </template>
+
+<style>
+@reference '@/styles/main.css';
+
+.ui-kit-input-container {
+  @apply flex flex-col gap-1.5;
+}
+
+.ui-kit-input-container--sm {
+  @apply rounded-3.5 px-3 py-2 text-sm;
+}
+
+.ui-kit-input-container--lg {
+  @apply rounded-5.5;
+}
+
+.ui-kit-input-container span {
+  @apply text-brown-500;
+}
+
+.ui-kit-input {
+  @apply bg-brown-100 rounded-5 w-full px-6 py-3;
+}
+
+.ui-kit-input input {
+  @apply border-brown-700 text-brown-700 placeholder:text-brown-500 w-full min-w-0 border-b border-dashed bg-transparent outline-none;
+}
+
+.ui-kit-input-container--text-left input {
+  @apply text-left;
+}
+
+.ui-kit-input-container--text-right input {
+  @apply text-right;
+}
+
+.ui-kit-input-container--text-center input {
+  @apply text-center;
+}
+
+.ui-kit-input-container--lg input {
+  @apply text-lg;
+}
+</style>
