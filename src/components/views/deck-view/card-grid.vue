83ad725e1011a15097c72e9eabc75809888b715a
Chris <jol.chris2@gmail.com>
2025-08-20 14:41:30 -0700

    TARO-72: Allow card upload

--- src/components/views/deck-view/card-grid.vue
+++ src/components/views/deck-view/card-grid.vue
@@ -1,33 +1,56 @@
 <script setup lang="ts">
 import Card from '@/components/card/index.vue'
 import { useAudio } from '@/composables/use-audio'
-import { nextTick, ref } from 'vue'
+import { ref } from 'vue'
+import { useCard } from '@/composables/use-card'
 
-const { mode, activeCardIndex } = defineProps<{
+const {
+  mode,
+  activeCardIndex,
+  side = 'front'
+} = defineProps<{
   cards: Card[]
   mode: 'edit' | 'view' | 'select'
+  side?: 'front' | 'back'
   activeCardIndex?: number
 }>()
 
 const emit = defineEmits<{
   (e: 'card-activated', index: number): void
+  (
+    e: 'card-image-updated',
+    card_id: number | undefined,
+    side: 'front' | 'back',
+    file: File | undefined
+  ): void
 }>()
 
 const audio = useAudio()
+const card_size = ref<'base' | 'xl'>('base')
+
+const get_front_image_url = (card: Card) => {
+  return useCard(card).front_image_url.value
+}
+
+const get_back_image_url = (card: Card) => {
+  return useCard(card).back_image_url.value
+}
 
 async function onCardClick(index: number) {
   if (activeCardIndex === index) return
 
+  card_size.value = 'base'
   audio.play('slide_up')
   emit('card-activated', index)
 
-  await nextTick()
+  await new Promise((resolve) => setTimeout(resolve, 1))
 
+  card_size.value = 'xl'
   const input = document.querySelector(
     '[data-testid="card-grid__selected-card"] .card-face__text-input'
   ) as HTMLInputElement
 
-  input.focus()
+  input?.focus()
 }
 
 function onCardMouseEnter(index: number) {
@@ -43,9 +66,13 @@ function onCardMouseEnter(index: number) {
       class="group"
       :key="card.id"
       :front_text="card.front_text"
+      :back_text="card.back_text"
+      :front_image_url="get_front_image_url(card)"
+      :back_image_url="get_back_image_url(card)"
       :mode="mode"
       @click="onCardClick(index)"
       @mouseenter="onCardMouseEnter(index)"
+      @image-uploaded="emit('card-image-updated', card?.id, side, $event.file)"
     >
       <div
         v-if="mode === 'edit'"
@@ -59,8 +86,11 @@ function onCardMouseEnter(index: number) {
         class="[&>.card-face]:shadow-modal !absolute top-1/2 left-1/2 z-10 -translate-1/2 [&>.card-face]:ring-2
           [&>.card-face]:ring-blue-500"
         :front_text="card.front_text"
+        :back_text="card.back_text"
+        :front_image_url="get_front_image_url(card)"
+        :back_image_url="get_back_image_url(card)"
         :mode="mode"
-        size="lg"
+        :size="card_size"
       ></card>
     </card>
   </div>
