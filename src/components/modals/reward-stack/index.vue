d1a60deef6bdbfd044e6783d0cf8737e39e545bf
Chris <jol.chris2@gmail.com>
2025-08-31 19:09:34 -0700

    TARO-76: build out UI

---
+++ src/components/modals/reward-stack/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Paperclips from './paperclips.vue'
+import RewardDialog from './reward-dialog.vue'
+
+const { rewards, score, total } = defineProps<{
+  rewards: Reward[]
+  score: number
+  total: number
+}>()
+
+const reward_components = {
+  paperclips: Paperclips
+}
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="drop-shadow-modal">
+      <component v-for="reward in rewards" :is="reward_components[reward.type]" :reward="reward" />
+    </div>
+
+    <reward-dialog
+      class="drop-shadow-overlay"
+      :score="score"
+      :total="total"
+      :num_rewards="rewards.length"
+      :first_reward="rewards[0]"
+    />
+  </div>
+</template>
---
+++ types/reward.d.ts
@@ -0,0 +1,5 @@
+declare type Reward = {
+  type: 'paperclips'
+  label: string
+  amount: number
+}
