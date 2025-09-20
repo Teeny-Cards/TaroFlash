49a296680e82b9beeec60e77e323cb44ca657b5e
Chris <jol.chris2@gmail.com>
2025-09-20 13:47:16 -0700

    tabs and nav-bar restyle

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,38 +1,12 @@
 <template>
-  <nav class="font-primary sticky top-0 z-10 flex py-2 w-full justify-center bg-blue-500">
-    <div class="flex w-full max-w-375 items-center justify-between px-4 sm:px-16">
-      <RouterLink to="/dashboard" class="flex items-center gap-2 text-3xl text-white">
-        <ui-kit:image src="taro-logo_3" class="max-w-6" />
-        <div>TaroCards</div>
-      </RouterLink>
-      <!-- <div class="flex items-end gap-6">
-        <div class="mt-6 flex gap-2.25">
-          <div
-            v-for="i in 5"
-            :key="i"
-            class="rounded-8 border-brown-100 h-17 w-17 border-6 bg-pink-400 ring-8 ring-blue-500"
-          ></div>
-        </div>
-        <div class="flex gap-4">
-          <ui-kit:button
-            icon-left="store"
-            icon-size="base"
-            icon-only
-            inverted
-            class="p-1.5 ring-8 ring-blue-500"
-            :class="{ 'ring-pink': isActive('shop') }"
-            @click="routeToShop"
-          ></ui-kit:button>
-          <ui-kit:button
-            icon-left="apps"
-            icon-only
-            icon-size="base"
-            inverted
-            class="relative p-1.5 ring-8 ring-blue-500"
-          >
-          </ui-kit:button>
-        </div>
-      </div> -->
+  <nav class="w-full sticky top-0 z-10 pb-4">
+    <div class="font-primary flex pt-4 pb-8 w-full justify-center bg-blue-500 wave-bottom-sm">
+      <div class="flex w-full max-w-375 items-center justify-between px-4 sm:px-16">
+        <RouterLink to="/dashboard" class="flex items-center gap-2 text-3xl text-white">
+          <ui-kit:image src="taro-logo_3" class="max-w-6" />
+          <div>TaroCards</div>
+        </RouterLink>
+      </div>
     </div>
   </nav>
 </template>
