5b22a73c9529a3da7004072018f4d91761c78498
Chris <jol.chris2@gmail.com>
2025-10-26 12:48:29 -0700

    TARO-44: tests

--- tests/integration/components/modals/move-cards.test.js
+++ tests/integration/components/modals/move-cards.test.js
@@ -7,8 +7,7 @@ import { deck } from '@tests/mocks/models/deck'
 const member_decks = deck.many(3)
 const mocks = vi.hoisted(() => {
   return {
-    fetchMemberDecks: vi.fn(() => member_decks),
-    close: vi.fn()
+    fetchMemberDecks: vi.fn(() => member_decks)
   }
 })
 
@@ -21,7 +20,7 @@ it('renders properly with default props', () => {
     props: {
       cards: [],
       current_deck_id: 0,
-      close: mocks.close
+      close: vi.fn()
     }
   })
 
@@ -34,7 +33,7 @@ it('renders properly with cards', () => {
     props: {
       cards,
       current_deck_id: 0,
-      close: mocks.close
+      close: vi.fn()
     }
   })
 
@@ -42,21 +41,117 @@ it('renders properly with cards', () => {
   expect(wrapper.find('[data-testid="move-cards__header"] h1').text()).toContain('3')
 })
 
-it('renders properly with current_deck_id', () => {
+it('renders properly with current_deck_id', async () => {
   const cards = card.many(3)
   const wrapper = mount(MoveCards, {
     props: {
       cards,
       current_deck_id: member_decks[0].id,
-      close: mocks.close
+      close: vi.fn()
     }
   })
 
-  console.log(wrapper.html())
+  await Promise.resolve() // Wait for async operations
+  await wrapper.vm.$nextTick() // Wait for component to update
 
-  expect(
-    wrapper
-      .findAll('[data-testid="move-cards__deck-list"] [data-testid="ui-kit-list-item"]')[0]
-      .attributes('disabled')
-  ).toBe('true')
+  const listItems = wrapper.findAll(
+    '[data-testid="move-cards__deck-list"] [data-testid="ui-kit-list-item"]'
+  )
+
+  expect(listItems.length).toBe(3)
+  expect(listItems[0].classes()).toContain('ui-kit-list-item--disabled')
+  expect(listItems[1].classes()).not.toContain('ui-kit-list-item--disabled')
+  expect(listItems[2].classes()).not.toContain('ui-kit-list-item--disabled')
+})
+
+it('calls close function when cancel button is clicked', async () => {
+  const close = vi.fn()
+  const wrapper = mount(MoveCards, {
+    props: {
+      cards: [],
+      current_deck_id: 0,
+      close
+    }
+  })
+
+  await wrapper.find('[data-testid="move-cards__cancel"]').trigger('click')
+
+  expect(close).toHaveBeenCalledWith(false)
+})
+
+it('calls close function with deck_id when move button is clicked', async () => {
+  const close = vi.fn()
+  const wrapper = mount(MoveCards, {
+    props: {
+      cards: [],
+      current_deck_id: 0,
+      close
+    }
+  })
+
+  await Promise.resolve() // Wait for async operations
+  await wrapper.vm.$nextTick() // Wait for component to update
+
+  await wrapper
+    .find('[data-testid="move-cards__deck-list"] [data-testid="ui-kit-list-item"]')
+    .trigger('click')
+  await wrapper.find('[data-testid="move-cards__move"]').trigger('click')
+
+  expect(close).toHaveBeenCalledWith({ deck_id: expect.any(Number) })
+})
+
+it('does not call close function with false when move button is clicked and no deck is selected', async () => {
+  const close = vi.fn()
+  const wrapper = mount(MoveCards, {
+    props: {
+      cards: [],
+      current_deck_id: 0,
+      close
+    }
+  })
+
+  await wrapper.find('[data-testid="move-cards__move"]').trigger('click')
+
+  expect(close).not.toHaveBeenCalledWith({ deck_id: expect.any(Number) })
+})
+
+it('sets deck_id when list item is clicked', async () => {
+  const wrapper = mount(MoveCards, {
+    props: {
+      cards: [],
+      current_deck_id: 0,
+      close: vi.fn()
+    }
+  })
+
+  await Promise.resolve() // Wait for async operations
+  await wrapper.vm.$nextTick() // Wait for component to update
+
+  await wrapper
+    .find('[data-testid="move-cards__deck-list"] [data-testid="ui-kit-list-item"]')
+    .trigger('click')
+
+  expect(wrapper.vm.selected_deck_id).toBe(member_decks[0].id)
+})
+
+it('unsets deck_id when list item is clicked again', async () => {
+  const wrapper = mount(MoveCards, {
+    props: {
+      cards: [],
+      current_deck_id: 0,
+      close: vi.fn()
+    }
+  })
+
+  await Promise.resolve() // Wait for async operations
+  await wrapper.vm.$nextTick() // Wait for component to update
+
+  await wrapper
+    .find('[data-testid="move-cards__deck-list"] [data-testid="ui-kit-list-item"]')
+    .trigger('click')
+  await wrapper
+    .find('[data-testid="move-cards__deck-list"] [data-testid="ui-kit-list-item"]')
+    .trigger('click')
+
+  expect(wrapper.vm.selected_deck_id).toBeUndefined()
 })
