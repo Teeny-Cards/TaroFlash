2b67a93073375594c6ba0c6799086451bee64520
Chris <jol.chris2@gmail.com>
2026-01-21 14:42:59 -0800

    privacy & terms

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,8 @@ import WelcomeView from '@/views/welcome/welcome-view.vue'
 import DeckView from '@/views/deck/deck-view.vue'
 import AuthenticatedView from '@/views/authenticated.vue'
 import CheckoutView from '@/views/checkout.vue'
+import PrivacyPolicyView from '@/views/privacy-policy.vue'
+import TermsOfServiceView from '@/views/terms-of-service.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/welcome',
       name: 'welcome',
       component: WelcomeView
+    },
+    {
+      path: '/privacy',
+      name: 'privacy-policy',
+      component: PrivacyPolicyView
+    },
+    {
+      path: '/terms',
+      name: 'terms-of-service',
+      component: TermsOfServiceView
     },
     {
       path: '/',
---
+++ src/views/terms-of-service.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="p-4 text-brown-700 dark:text-brown-100">
+    <h1 class="text-5xl font-bold mb-4">{{ t('terms-of-service.title') }}</h1>
+    <p class="text-lg mb-8">{{ t('terms-of-service.description') }}</p>
+  </div>
+</template>
