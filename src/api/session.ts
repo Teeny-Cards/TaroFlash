60c9debbbe0c5783bceed76e0098416179e0f292
Chris <jol.chris2@gmail.com>
2026-01-24 21:01:07 -0800

    add redirectTo env var

--- src/api/session.ts
+++ src/api/session.ts
@@ -66,7 +66,13 @@ export async function signInOAuth(
   provider: OAuthProvider,
   options?: SignupOAuthOptions
 ): Promise<void> {
-  const { error } = await supabase.auth.signInWithOAuth({ provider, options })
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: import.meta.env.VITE_AUTH_PUBLIC_URL,
+      ...options
+    }
+  })
 
   if (error) {
     throw error
