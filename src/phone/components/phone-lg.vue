a5df3fbd0af253a5f5346c79caa7bd1fc1c54545
Chris <jol.chris2@gmail.com>
2026-01-25 08:41:30 -0800

    initial app settings refactor

--- src/phone/components/phone-lg.vue
+++ src/phone/components/phone-lg.vue
@@ -18,7 +18,8 @@ const current_app = computed(() => {
 <template>
   <div
     data-testid="phone"
-    class="pointer-events-auto w-223.75 h-150 bg-brown-300 dark:bg-grey-700 rounded-16 shadow-sm"
+    class="pointer-events-auto w-223.75 h-150 bg-brown-300 dark:bg-grey-800 rounded-10 shadow-sm
+      overflow-hidden"
   >
     <component v-if="current_app" :is="current_app.component" @close="emit('close')" />
   </div>
