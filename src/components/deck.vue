1268ea84749a653fc442ce2786c53cb35dc2870d
Chris <jol.chris2@gmail.com>
2026-02-01 10:14:38 -0800

    refactor deck/dashboard for new design

--- src/components/deck.vue
+++ src/components/deck.vue
@@ -1,55 +1,16 @@
 <script setup lang="ts">
-import Card from '@/components/card/index.vue'
-import UiButton from '@/components/ui-kit/button.vue'
-import StudyModal from './modals/study-session/index.vue'
-import { useModal } from '@/composables/modal'
-import { useI18n } from 'vue-i18n'
-
-const { deck, size = 'base' } = defineProps<{
-  deck: Deck
-  size?: 'xs' | 'sm' | 'base'
-  due?: boolean
-}>()
-
-const { t } = useI18n()
-const modal = useModal()
-
-function onStudyClicked() {
-  modal.open(StudyModal, {
-    backdrop: true,
-    props: {
-      deck
-    },
-    openAudio: 'ui.double_pop_up',
-    closeAudio: 'ui.double_pop_down'
-  })
-}
+defineProps<{ deck: Deck }>()
 </script>
 
 <template>
-  <div data-testid="deck" class="relative flex w-max flex-col gap-2.5 group">
-    <card :size="size" class="relative cursor-pointer" :sfx="{ hover: 'ui.click_04' }">
-      <div
-        v-if="due && deck.due_count"
-        class="outline-brown-100 dark:outline-grey-900 absolute -top-2 -right-2 flex h-7.5 w-7.5
-          items-center justify-center rounded-full bg-red-500 outline-4"
-      >
-        <h2 class="text-base text-white">{{ deck.due_count }}</h2>
-      </div>
+  <div data-testid="deck" class="relative cursor-pointer">
+    <div
+      class="aspect-card w-48 rounded-10 ring-6 border-3 border-purple-400 ring-purple-400
+        bg-purple-400"
+    ></div>
 
-      <!-- <div
-        v-if="due"
-        class="hidden absolute h-full w-full group-hover:flex pointer-coarse:flex justify-center
-          items-center"
-      >
-        <ui-button icon-left="play" @click.stop="onStudyClicked" theme="brown" icon-only>{{
-          t('common.study')
-        }}</ui-button>
-      </div> -->
-    </card>
-
-    <div>
-      <h2 class="text-md text-brown-700 dark:text-brown-300">{{ deck.title }}</h2>
+    <div class="absolute w-full -bottom-2.5 bg-brown-300 px-3 py-2 rounded-4.5">
+      <h2 class="text-lg text-center text-brown-700">{{ deck.title }}</h2>
     </div>
   </div>
 </template>
